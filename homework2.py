4527b92d474a7869b5b709573b3b68c3cba5a3a7
Oomnisphere <87091874+Oomnisphere@users.noreply.github.com>
2024-07-12 16:56:31 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homeworks=12
+time_all=1.5
+name='Python'
+time1=time_all/homeworks
+print('Курс:',name,', ','Всего задач:',homeworks,'Среднее время выполнения:',time1) #корявость вставки запятых. не решил((
